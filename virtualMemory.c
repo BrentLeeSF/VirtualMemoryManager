ebf6351865eaf669d85c77ebb0bb0940bf6b9b84
Brent Rucker <brent.rucker@gmail.com>
2017-12-25 13:31:09 -0800

    final version of virtual memory

--- virtualMemory.c
+++ virtualMemory.c
@@ -6,11 +6,11 @@
 The goal of this project was to write a program that translates 
 logical address to physical address for a virtual address space 
 of 2^16 (65,536) bytes. We read in the file containing logical 
-addresses, using a TLB as well as a page table, and translated 
-each logical address to its corresponding physical address and 
-output the value of the byte stored at the translated physical 
-address. Then print out these values and ratios (page fault rate 
-and TLB rate).
+addresses, using a TLB (translation look-aside buffer) as well 
+as a page table, and translated each logical address to its 
+corresponding physical address and output the value of the byte 
+stored at the translated physical address. Then print out these 
+values and ratios (page fault rate and TLB rate).
 */
 
 
@@ -36,8 +36,6 @@ char readBacker[FRAMELENGTH];
 int physicalMemory[physicalMemoryBytes];
 int pageFault = 0;
 
-
-
 int changeAddress(int logAddress);
 int readBackStore(int page);
 void initializeInfo(int * arr, int n);
@@ -45,43 +43,50 @@ void initializeInfo(int * arr, int n);
 
 int main(int argc, char *argv[]) {
 
+	int translations = 0, logAddress = 0, address = 0;
+	char line[LINELENGTH];
+
 	if(argc != 2) {
 		printf("Please enter two arguements.\nEx: ./file addresses.txt\n");
 	}
 
-	// Open Files
+	/* Open Files */
 	backStore = fopen("BACKING_STORE.bin", "r");
+
 	if(backStore == NULL) {
+
 		printf("1 Null\n");
 		return -1;
 	}
 
 	addressFile = fopen(argv[1], "r");
+
 	if(addressFile == NULL) {
+
 		printf("2 Null\n");
 		return -1;
 	}
 
-	// Initialize arrays
+	/* Initialize arrays */
 	initializeInfo(pageTable, PAGESIZE);
 	initializeInfo(pageFrame, PAGESIZE);
 	initializeInfo(TLBPage, TLB_LENGTH);
 	initializeInfo(TLBFrame, TLB_LENGTH);
 
-	int translations = 0;
-	char line[LINELENGTH];
-
-	// Go through each line of address file and pass logical address
-	// to Change address, which will translate the ino to a physical 
-	// address
+	
+	/* Go through each line of address file and pass logical address
+	to Change address, which will translate the ino to a physical address */
 	while(fgets(line, LINELENGTH, addressFile) != NULL) {
-		int logAddress = atoi(line);
-		int address = changeAddress(logAddress);
+
+		logAddress = atoi(line);
+		address = changeAddress(logAddress);
+
 		printf("Logical Address: %d, Physical Memory: %d, Value: %d\n", logAddress, address, physicalMemory[address]);
 		translations++;
+
 	}
 
-	// Print out results
+	/* Print out results */
 	printf("\n*** Final Info ***\n");
 	printf("Number of translations: %d\n", translations);
 	printf("Number of Page Faults: %d\n", pageFault);
@@ -89,7 +94,7 @@ int main(int argc, char *argv[]) {
 	printf("Number of TLB Hits: %d\n", TLBNum);
 	printf("TLB Rate: %f\n", (float)TLBNum/(float)translations);
 
-	// Close files
+	/* Close files */
 	fclose(addressFile);
 	fclose(backStore);
 
@@ -97,7 +102,9 @@ int main(int argc, char *argv[]) {
 }
 
 void initializeInfo(int *arr, int n) {
-	int i;
+
+	int i = 0;
+
 	for(i = 0; i < n; i++) {
 		arr[i] = -1;
 	}
@@ -113,40 +120,48 @@ We then return the physical memory address.
 */
 int changeAddress(int logAddress) {
 
-	int page = logAddress/PAGESIZE;
 
-	double origPage, decPage, intPage;
+	int page = 0, i = 0, frameNum = -1, offset = 0;
+	double origPage, decPage, intPage, offsetDub = 0.0;
+
+	page = logAddress/PAGESIZE;
 
 	origPage = (double)logAddress/PAGESIZE;
 	decPage = modf(origPage, &intPage);
-	double offsetDub = decPage * PAGESIZE;
-	int offset = (int)offsetDub;
-	//printf("Page: %d, offSet: %d\n", page, offset);
 
-	int frameNum = -1;
+	offsetDub = decPage * PAGESIZE;
+	offset = (int)offsetDub;
 
-	int i;
-	// check if in TLB frame
+
+	/* check if page is in TLB frame */
 	for(i = 0; i< TLB_LENGTH; i++) {
+
 		if(TLBPage[i] == page) {
+
 			frameNum = TLBFrame[i];
 			TLBNum++;
 		}
+
 	}
 
+	/* if page was not in TLB, read from BACK_STORE, or
+	get page from pageTable */
 	if(frameNum == -1) {
-		// if not in either, page fault
+
+		/* if not in either, page fault */
 		if(pageTable[page] == -1) {
+
 			frameNum = readBackStore(page);
-		}
-		else {
-			// if not in TLB frame, get from pageTable
+
+		} else {
+			/* if not in TLB frame, get from pageTable */
 			frameNum = pageTable[page];
 		}
 
 		TLBPage[TLBCounter%TLB_LENGTH] = page;
 		TLBFrame[TLBCounter%TLB_LENGTH] = frameNum;
 		TLBCounter++;
+
 	}
 
 	return (frameNum * PAGESIZE) + offset;	
@@ -160,7 +175,10 @@ memory array. Next we insert the frame into the page table and
 increase the page faults. Finally we return the frame we used.
 */
 int readBackStore(int page) {
-	// SEEK_SET is in fseek() - it seeks from the beginning of the file
+
+	int i = 0, j = 0, availableFrame = 0, startFrameIndex = 0;
+
+	/* SEEK_SET is in fseek() - it seeks from the beginning of the file */
 	if(fseek(backStore, page * PAGESIZE, SEEK_SET) != 0) {
 		printf("ERROR\n");
 	}
@@ -169,28 +187,33 @@ int readBackStore(int page) {
 		printf("ERROR\n");
 	}
 
-	// Get available frame by looking for unused index in pageFrame 
-	int i;
-	int availableFrame;
+	/* Get available frame by looking for unused index in pageFrame */
 	for(i =0; i < PAGESIZE; i++) {
+
 		if(pageFrame[i] == -1) {
+
 			pageFrame[i] = 0;
 			availableFrame = i;
 			break;
 		}
+
 	}
-	// Start at specific index for each frame
-	int startFrameIndex = PAGESIZE * availableFrame;
-	int j;
+
+	/* Start at specific index for each frame */
+	startFrameIndex = PAGESIZE * availableFrame;
+
 	for(j = 0; j < PAGESIZE; j++) {
+
 		physicalMemory[startFrameIndex] = readBacker[j];
 		startFrameIndex++;
+
 	}
 
 	pageTable[page] = availableFrame;
 	pageFault++;
 	
 	return availableFrame;
+
 }
 
 
